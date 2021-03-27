6ed96dad03d45579d478a2955f24f31c1d95bf73
Srikanth Anantharaman <mailtosrik@gmail.com>
2021-03-27 02:34:32 -0400

    First commit

---
+++ my_first_core_mvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_first_core_mvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ my_first_core_mvc/Data/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using my_first_core_mvc_movie.Models;
+
+namespace my_first_core_mvc_movie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
